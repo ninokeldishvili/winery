473581252f7f2a526a919f054197da7a96c65785
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-11-23 15:04:37 +0400

    Red Wines page title

--- app/pages/wine/red-wines.vue
+++ app/pages/wine/red-wines.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div class="text-center py-20">
+    <h1
+      class="font-georgian text-5xl md:text-7xl uppercase tracking-widest text-wine-red"
+    >
+      <span class="relative inline-block mr-1">
+        <span
+          class="absolute inset-0 bg-wine-white/20 blur-2xl rounded-full scale-150"
+        ></span>
 
-<style scoped></style>
+        <span class="relative z-10 text-wine-white">D</span>
+      </span>
+      <span class="relative z-10">epth</span>
+
+      <span
+        class="text-4xl md:text-6xl align-middle mx-2 font-tangerine lowercase text-primary"
+        >of</span
+      >
+      Tradition
+    </h1>
+
+    <p class="mt-4 text-lg text-primary font-georgian italic">
+      {{ $t("explore-bold-textures") }}
+    </p>
+  </div>
+</template>
